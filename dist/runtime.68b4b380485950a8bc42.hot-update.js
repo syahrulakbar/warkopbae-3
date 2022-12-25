602f123c96a3517c4e5f5d44055248c1a23dcaf8
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2022-12-25 22:44:49 +0700

    #4 Fix CLS

---
+++ dist/runtime.68b4b380485950a8bc42.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdatewarkopbae_restaurant_apps"]("runtime",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("c322438cdc52e982fbe4")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicnVudGltZS42OGI0YjM4MDQ4NTk1MGE4YmM0Mi5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O1VBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93YXJrb3BiYWUtcmVzdGF1cmFudC1hcHBzL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJzb3VyY2VzQ29udGVudCI6WyJfX3dlYnBhY2tfcmVxdWlyZV9fLmggPSAoKSA9PiAoXCJjMzIyNDM4Y2RjNTJlOTgyZmJlNFwiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
